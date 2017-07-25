b906282c613f03b5a109db4e2ea37bf07f267e2a
Ali Haq <ali.m.haq@gmail.com>
2017-07-25 11:14:40 -0700

    Fixed typo

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 
 import { logout } from '../../actions/session_actions';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 
 const mapStateToProps = ({ session }) => ({
   currentUser: session.currentUser
